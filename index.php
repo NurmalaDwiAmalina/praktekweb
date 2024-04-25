145a6c39b00b4a138a3d09cbe8e0c9ac094d6b99
amaalina <nurmaladwiamalina@gmail.com>
2024-04-25 14:53:27 +0700

    UTS PEMROGRAMAN WEB

--- index.php
+++ index.php
@@ -197,9 +197,8 @@ if(isset($_SESSION['login'])){
     </div>
     <div class="from-floating">
       <input name="umur" class="form-control" placeholder="Umur">
-      <label for="floatingInput"></label>
+      <label for="floatingInput">Umur</label>
     </div>
-    <br>
     <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
   </form>
